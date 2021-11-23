e1908df878f5e955c51d1a599dc5f25f5d941745
gn-t-k <gntk1995422@gmail.com>
2021-11-23 14:00:32 +0900

    storybook設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
@@ -6,5 +8,15 @@ module.exports = {
   },
   typescript: {
     reactDocgen: false,
+  },
+  webpackFinal: (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+
+    return config;
   },
 };
